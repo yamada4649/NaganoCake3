5c2346339df1e9f52d68140fe8654a60753771ea
yamada4649 <ryuki.yamada4649@gmail.com>
2021-01-10 17:40:18 +0900

    favorite_controllerに変数を追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,14 +1,16 @@
 class Public::FavoritesController < ApplicationController
 	def create
 		 @item = Item.find(params[:item_id])#ここの修正をするためにgit resetをしました。
-		 favorite = @item.favorites.new(end_user_id: current_end_user.id)
-		 favorite.save
+		 @favorite = @item.favorites.new(end_user_id: current_end_user.id)
+		 @favorite.save
+		 @favorite
 		 redirect_to request.referer
 	end
 	def destroy
 		@item = Item.find(params[:item_id])
-		favorite = @item.favorites.find_by(end_user_id: current_end_user.id)
-		favorite.destroy
+		@favorite = @item.favorites.find_by(end_user_id: current_end_user.id)
+		@favorite
+		@favorite.destroy
 		redirect_to request.referer
 	end
 end
